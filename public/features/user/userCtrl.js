a088ecf3121a35c464590f816f42419f4ea82ec6
Scott Peterson <stpeterson28@yahoo.com>
2016-05-15 18:35:49 -0600

    Editing works. Only opens the options on the photo I select for edit and if user hits cancel after changing a value the value is reverted. Finally.

--- public/features/user/userCtrl.js
+++ public/features/user/userCtrl.js
@@ -2,16 +2,15 @@ angular.module('scrapsApp')
 	.controller('userCtrl', function ($scope, currentUser, $stateParams, userInfo, postSvc, userSvc) {
 
 		$scope.currentUser = currentUser; //Current user.
-		$scope.userInfo = userInfo;	//Info of user whose page someone is on.
+		$scope.userInfo = userInfo; //Info of user whose page someone is on.
 
 		$scope.newPhoto = {}; //create an object on scope that is used by the photoUpload directive.
-		$scope.photos = $scope.userInfo.photos;  //put user photos on scope.
+		$scope.photos = $scope.userInfo.photos; //put user photos on scope.
+		$scope.photo = {};
 
 		//By default assume there will be no user logged in.
 		$scope.loginBtn = true;
 		$scope.signUpBtn = true;
-		$scope.editingPhotoBtn = false;
-		$scope.editOptions = false;
 
 		//Number of posts a user has made. Show the stat by default.
 		$scope.postCount = $scope.userInfo.photos.length;
@@ -27,7 +26,7 @@ angular.module('scrapsApp')
 
 		//If the current user is on their own page, these options are accessible.
 		if ($stateParams.username === currentUser.username) {
-			$scope.editingPhotoBtn = true;
+			//			$scope.userPhotos.photoEditorEnabled = true;
 			$scope.uploadProfilePicBtn = true;
 			$scope.uploadPhotoBtn = true;
 			$scope.deletePhotoBtn = true;
@@ -38,22 +37,25 @@ angular.module('scrapsApp')
 			$scope.postStats = false;
 		};
 
-		$scope.getPhotos = function() {
-			userSvc.getUsersPhotos($scope.currentUser.username).then(function(response) {
+		$scope.getPhotos = function () {
+			userSvc.getUsersPhotos($scope.currentUser.username).then(function (response) {
 				return $scope.photos = response;
 			})
 		}
 
 		//Show the edit options when clicked. Hide the editingBtn.
-		$scope.editingPhoto = function (index) {
-			$scope.editOptions = true;
-			$scope.editingPhotoBtn = false;
+		$scope.editPhotoEnabled = function (index) {
+			$scope.photo = $scope.photos[index]
+			$scope.photo.photoEditorEnabled = true;
+			$scope.photo.photoEditOptions = true;
+			console.log($scope.photo);
 		};
 
 		//Cancels editing and returns the prior scope. Well that's the plan, but it's broken.
-		$scope.resetPhoto = function (index) {
-			$scope.editOptions = false;
-			$scope.editingPhotoBtn = true;
+		$scope.cancelEditingPhoto = function (index) {
+			$scope.photo.photoEditorEnabled = false;
+			$scope.photo.photoEditOptions = false;
+			$scope.getPhotos();
 		};
 
 		//Updates a photo if user changes details about it.
@@ -62,9 +64,8 @@ angular.module('scrapsApp')
 		};
 
 		//Delete a photo from a user and the database.
-		$scope.deletePhoto = function(photoId) {
+		$scope.deletePhoto = function (photoId) {
 			postSvc.deletePhoto(photoId);
-
 			$scope.getPhotos();
 		}
 
